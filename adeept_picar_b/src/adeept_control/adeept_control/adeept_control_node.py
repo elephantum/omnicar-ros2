da77b15072e03a8a0a746145e27e69944d42a348
Andrey Tatarinov <a@tatarinov.co>
2023-12-11 21:04:12 +0000

    head movement

--- adeept_picar_b/src/adeept_control/adeept_control/adeept_control_node.py
+++ adeept_picar_b/src/adeept_control/adeept_control/adeept_control_node.py
@@ -1,16 +1,19 @@
-import time
 from rclpy.node import Node
 import rclpy
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist
 from board import SCL, SDA
 import busio
 from adafruit_motor import servo
 import adafruit_pca9685
 import RPi.GPIO as GPIO
 
-Motor_A_EN    = 17
-Motor_A_Pin1  = 27
-Motor_A_Pin2  = 18
+Motor_A_EN = 17
+Motor_A_Pin1 = 27
+Motor_A_Pin2 = 18
+
+
+def clip(x, lower, upper):
+    return max(min(x, upper), lower)
 
 
 class AdeeptControlNode(Node):
@@ -37,27 +40,36 @@ class AdeeptControlNode(Node):
         self.drive_motor.ChangeDutyCycle(0)
 
         self.cmd_vel_sub = self.create_subscription(
-            Twist, "cmd_vel", self.cmd_vel_callback, 10
+            Twist,
+            "cmd_vel",
+            self.cmd_vel_callback,
+            3,
+        )
+
+        self.cmd_head_sub = self.create_subscription(
+            Twist,
+            "cmd_head",
+            self.cmd_head_callback,
+            3,
         )
 
     def accelerate(self, speed: float) -> None:
         if speed > 0:
             GPIO.output(Motor_A_Pin1, GPIO.LOW)
             GPIO.output(Motor_A_Pin2, GPIO.HIGH)
-            self.drive_motor.ChangeDutyCycle(int(speed * 100))
+            self.drive_motor.ChangeDutyCycle(clip(int(speed * 100), 0, 100))
         else:
             GPIO.output(Motor_A_Pin1, GPIO.HIGH)
             GPIO.output(Motor_A_Pin2, GPIO.LOW)
-            self.drive_motor.ChangeDutyCycle(int(-speed * 100))
-
-    def steer(self, steering: float) -> None:
-        angle = int(steering * 90 + 90)
-        self.steering_servo.angle = angle
+            self.drive_motor.ChangeDutyCycle(clip(int(-speed * 100), 0, 100))
 
     def cmd_vel_callback(self, msg: Twist) -> None:
-        self.accelerate(msg.linear.x)
-        self.steer(msg.angular.z)
+        self.accelerate(msg.linear.x * 1.5)
+        self.steering_servo.angle = int(msg.angular.z * 90 + 90)
 
+    def cmd_head_callback(self, msg: Twist) -> None:
+        self.head_yaw_servo.angle = int((msg.angular.z * 90) + 90)
+        self.head_pitch_servo.angle = int((msg.angular.y * 45) + 90)
 
 
 def main(args=None):
